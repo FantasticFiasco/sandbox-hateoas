0c8d040b5c47a56d0391d7d41d464c9caf32fc48
FantasticFiasco <mattias.kindborg@gmail.com>
2018-03-14 22:32:44 +0100

    wip

---
+++ src/hal/dotnet-and-hal-browser/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Halcyon.Web.HAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hateoas.Controllers
+{
+    [Route("api")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.CreateHalResponse()
+                .AddSelfLink(Request)
+                .AddLink(LinkTemplates.Author.GetAll)
+                .AddLink(LinkTemplates.Article.GetAll)
+                .ToActionResult(this);
+        }
+    }
+}
