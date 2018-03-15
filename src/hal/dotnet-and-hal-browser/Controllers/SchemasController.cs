2774dcf203f736d34be834c9d49ac7f7842f0882
FantasticFiasco <mattias.kindborg@gmail.com>
2018-03-15 21:51:21 +0100

    wip

---
+++ src/hal/dotnet-and-hal-browser/Controllers/SchemasController.cs
@@ -0,0 +1,26 @@
+﻿using Hateoas.Controllers.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+
+namespace Hateoas.Controllers
+{
+    public class SchemasController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{entity:regex(.*)}/schema")]
+        public string Get(string entity)
+        {
+            JSchemaGenerator generator = new JSchemaGenerator();
+
+            switch (entity)
+            {
+                case "author":
+                    JSchema schema = generator.Generate(typeof(AuthorBody));
+                    return schema.ToString();
+            }
+
+            return entity;
+        }
+    }
+}
